c207b8e05e769d3c8294eeefd4a9d8da65a652b6
Carlos <carlosjorgech7@gmail.com>
2025-03-12 14:00:28 +0100

    fix

--- src/app/shared/interceptors/spinner-f.interceptor.ts
+++ src/app/shared/interceptors/spinner-f.interceptor.ts
@@ -3,6 +3,7 @@ import { inject } from '@angular/core';
 import { SpinnerService } from '@shared/services/spinner.service';
 import { finalize } from 'rxjs';
 
+// Functional
 export const spinnerInterceptor: HttpInterceptorFn = (req, next) => {
   const spinnerService = inject(SpinnerService);
   spinnerService.show();
