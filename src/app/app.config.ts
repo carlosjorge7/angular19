d40b890eec52a20239f13d003a0b70efb8675a83
Carlos <carlosjorgech7@gmail.com>
2025-03-12 13:58:24 +0100

    wip

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,28 @@
 import { registerLocaleData } from '@angular/common';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import localeEs from '@angular/common/locales/es';
 import {
   ApplicationConfig,
-  importProvidersFrom,
   LOCALE_ID,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-
-import { SpinnerInterceptor } from '@shared/interceptors/spinner.interceptor';
 import { provideToastr } from 'ngx-toastr';
 import { appRoutes } from './app.routes';
+import { spinnerInterceptor } from '@shared/interceptors/spinner-f.interceptor';
 
 registerLocaleData(localeEs, 'es');
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(HttpClientModule),
     { provide: LOCALE_ID, useValue: 'es' },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: SpinnerInterceptor,
-      multi: true,
-    },
+    provideHttpClient(withInterceptors([spinnerInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right',
---
+++ src/app/shared/interceptors/spinner-f.interceptor.ts
@@ -0,0 +1,10 @@
+import type { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { SpinnerService } from '@shared/services/spinner.service';
+import { finalize } from 'rxjs';
+
+export const spinnerInterceptor: HttpInterceptorFn = (req, next) => {
+  const spinnerService = inject(SpinnerService);
+  spinnerService.show();
+  return next(req).pipe(finalize(() => spinnerService.hide()));
+};
