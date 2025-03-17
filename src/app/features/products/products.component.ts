9ff7405411345472c7f38bf9f28db37debc64368
Carlos <carlosjorgech7@gmail.com>
2025-03-17 18:38:44 +0100

    to signal

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,15 +1,15 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CartStateService } from 'src/app/store/cart-state/cart-state.service';
 import { CardComponent } from '@features/products/card/card.component';
 import { Product } from '@features/products/product.interface';
 import { ProductsService } from '@features/products/products.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CardComponent, AsyncPipe],
+  imports: [CardComponent],
   styleUrl: './products.component.scss',
   templateUrl: 'products.component.html',
 })
@@ -18,7 +18,7 @@ export default class ProductsComponent implements OnInit {
   private readonly _productsService = inject(ProductsService);
   private readonly _cartService = inject(CartStateService);
 
-  products$ = this._productsService.products$;
+  products$ = toSignal(this._productsService.products$);
 
   ngOnInit() {
     this._route.queryParams.subscribe((params) => {
